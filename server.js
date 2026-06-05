5eebb336d5e37d56321de3be593acde24281750e
郭睿宏 <hank@guoruihongdeMacBook-Air.local>
2026-06-06 00:04:21 +0800

    fix: server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ async function initCabinets() {
 async function initAdminAccount() {
     if (await Admin.countDocuments() === 0) {
         // 🔑 在這裡把 'admin123' 換成你的新密碼
-        const hashedPassword = await bcrypt.hash('admin111', 10);
+        const hashedPassword = await bcrypt.hash('library-tpcu', 10);
         
         // 👤 在這裡把 'admin' 換成你的新帳號
         await Admin.create({ username: 'library', password: hashedPassword });
